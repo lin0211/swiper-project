66df09856e08b90e27e05e52343197c96c6d3335
서재원 <cinos81@nhnlink.co.kr>
2024-01-02 14:22:28 +0900

    [feat] 데이터에 슬라이드 템플릿 정보를 이동

--- js/data.js
+++ js/data.js
@@ -131,3 +131,7 @@ export const modalData = {
     imgAlt: "눈물 흘리는 토끼",
   },
 };
+
+export const slideTemplateOrder = [
+  3, 1, 3, 2, 3, 2, 1, 3, 5, 4, 4, 3, 4, 3, 1, 1, 1, 5, 2, 3, 3, 3, 4, 1, 2,
+];
