8736184ebd2ac076d87e01191d9e0989ffb7128a
서재원 <cinos81@nhnlink.co.kr>
2024-01-02 14:24:22 +0900

    [feat] 초가화 시퀀스만 별도의 관심사로 분리

--- index.js
+++ index.js
@@ -1,67 +1,19 @@
-import {
-  answer,
-  insertLast,
-  slidesTemplate,
-  swiperSetting,
-  moveBunny,
-  modalSetting
-} from "./js/index.js";
-import * as nodes from './js/nodes.js'
+import { slideTemplateOrder } from "./js/data.js";
+import { insertLast } from "./js/insert.js";
+import { slidesTemplate } from "./js/template.js";
+import * as nodes from "./js/nodes.js";
+import * as handlers from "./js/handlers.js";
+import * as swiper from "./js/swiper.js";
 
-const swipers = [];
-
-const slideTemplateOrder = [
-  3, 1, 3, 2, 3, 2, 1, 3, 5, 4, 4, 3, 4, 3, 1, 1, 1, 5, 2, 3, 3, 3, 4, 1, 2,
-];
-
-slideTemplateOrder.forEach((item, index) => {
-  insertLast(nodes.box[index], slidesTemplate(item));
+document.addEventListener("DOMContentLoaded", () => {
+  slideTemplateOrder.forEach((item, index) => {
+    insertLast(nodes.box[index], slidesTemplate(item));
+  });
+  nodes.box.forEach((_, index) => {
+    const boxNum = `#swiper${index + 1}`;
+    swiper.list[index] = new window.Swiper(boxNum, swiper.setting);
+  });
+  nodes.completeButton.addEventListener("click", handlers.routeCheck);
+  nodes.modalRestartButton.addEventListener("click", handlers.restart);
+  nodes.modalCloseButton.addEventListener("click", handlers.close);
 });
-
-nodes.box.forEach((_, index) => {
-  const boxNum = `#swiper${index + 1}`;
-  swipers[index] = new window.Swiper(boxNum, swiperSetting);
-});
-
-const handleRouteCheck = (e) => {
-  e.preventDefault();
-
-  e.target.setAttribute("disabled", "");
-  swipers.forEach((swiper) => (swiper.allowTouchMove = false));
-
-  let isAnswer = true;
-  for (let i = 0; i < swipers.length; i++) {
-    if (!answer[`swipers[${i}]`].includes(swipers[i].realIndex)) {
-      isAnswer = false;
-      break;
-    }
-  }
-  if (isAnswer) {
-    // 애니메이션이 끝난 후 성공 모달창 활성화 시도
-    // moveBunny().then(() => {
-    //   modalSetting("success", modalText, modalImage, modal);
-    // });
-    moveBunny();
-    modalSetting("success", nodes.modalText, nodes.modalImage, nodes.modal, true);
-  } else {
-    // modalSetting("fail", modalText, modalImage, modal);
-    modalSetting("fail", nodes.modalText, nodes.modalImage, nodes.modal, false);
-  }
-};
-
-nodes.completeButton.addEventListener("click", handleRouteCheck);
-
-const handleRestart = () => {
-  window.location.reload();
-  nodes.completeButton.removeAttribute("disabled");
-  swipers.forEach((swiper) => (swiper.allowTouchMove = true));
-};
-
-const handleClose = () => {
-  nodes.modal.style.display = "none";
-  nodes.completeButton.removeAttribute("disabled");
-  swipers.forEach((swiper) => (swiper.allowTouchMove = true));
-};
-
-nodes.modalRestartButton.addEventListener("click", handleRestart);
-nodes.modalCloseButton.addEventListener("click", handleClose);
