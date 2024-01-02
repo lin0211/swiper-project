f33eb52b6465ace483f2891c0d26e5ec05252997
서재원 <cinos81@nhnlink.co.kr>
2024-01-02 13:42:34 +0900

    [feat] html 노드의 참조를 별도의 파일에 분리

--- index.js
+++ index.js
@@ -4,27 +4,21 @@ import {
   slidesTemplate,
   swiperSetting,
   moveBunny,
-  modalSetting,
+  modalSetting
 } from "./js/index.js";
+import * as nodes from './js/nodes.js'
 
 const swipers = [];
-const box = document.querySelectorAll(".box");
-const completeButton = document.querySelector(".completeButton");
+
 const slideTemplateOrder = [
   3, 1, 3, 2, 3, 2, 1, 3, 5, 4, 4, 3, 4, 3, 1, 1, 1, 5, 2, 3, 3, 3, 4, 1, 2,
 ];
-const modal = document.querySelector(".modal-wrapper");
-const modalRestartButton = document.querySelector(".modalRestartButton");
-const modalCloseButton = document.querySelector(".modalCloseButton");
-
-const modalText = document.querySelector(".modalText");
-const modalImage = document.querySelector(".modalImage");
 
 slideTemplateOrder.forEach((item, index) => {
-  insertLast(box[index], slidesTemplate(item));
+  insertLast(nodes.box[index], slidesTemplate(item));
 });
 
-box.forEach((_, index) => {
+nodes.box.forEach((_, index) => {
   const boxNum = `#swiper${index + 1}`;
   swipers[index] = new window.Swiper(boxNum, swiperSetting);
 });
@@ -48,26 +42,26 @@ const handleRouteCheck = (e) => {
     //   modalSetting("success", modalText, modalImage, modal);
     // });
     moveBunny();
-    modalSetting("success", modalText, modalImage, modal, true);
+    modalSetting("success", nodes.modalText, nodes.modalImage, nodes.modal, true);
   } else {
     // modalSetting("fail", modalText, modalImage, modal);
-    modalSetting("fail", modalText, modalImage, modal, false);
+    modalSetting("fail", nodes.modalText, nodes.modalImage, nodes.modal, false);
   }
 };
 
-completeButton.addEventListener("click", handleRouteCheck);
+nodes.completeButton.addEventListener("click", handleRouteCheck);
 
 const handleRestart = () => {
   window.location.reload();
-  completeButton.removeAttribute("disabled");
+  nodes.completeButton.removeAttribute("disabled");
   swipers.forEach((swiper) => (swiper.allowTouchMove = true));
 };
 
 const handleClose = () => {
-  modal.style.display = "none";
-  completeButton.removeAttribute("disabled");
+  nodes.modal.style.display = "none";
+  nodes.completeButton.removeAttribute("disabled");
   swipers.forEach((swiper) => (swiper.allowTouchMove = true));
 };
 
-modalRestartButton.addEventListener("click", handleRestart);
-modalCloseButton.addEventListener("click", handleClose);
+nodes.modalRestartButton.addEventListener("click", handleRestart);
+nodes.modalCloseButton.addEventListener("click", handleClose);
